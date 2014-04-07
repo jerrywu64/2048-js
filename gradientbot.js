1fce6e9e8de9bd99a119a8d302bc899f371a35cc
randomguy64 <64.randomguy@gmail.com>
2014-04-07 00:34:44 -0700

    added 4-tiles back in

--- gradientbot.js
+++ gradientbot.js
@@ -37,21 +37,21 @@ function Ai() {
 			sum += 9 * best;
 			moves += 9;
 			//console.log('b');
-			/*grid.cells[tile.x][tile.y].value = 4;
+			grid.cells[tile.x][tile.y].value = 4;
 			//grid.removeTile(tile);
 			//var tile2 = new Tile(cells[i], 4);
 			//grid.insertTile(tile2);
 			//console.log('c');
+			best = 0;
 			for (var j = 0; j < 4; j++) {
 				var next = grid.copy();
-				var best = 0;
 				if (next.move(j)) {
 					var value = lookahead(next, depth + 1);
 					if (value > best) best = value;
 				}
-				sum += best;
-				moves++;
-			}*/
+			}
+			sum += best;
+			moves++;
 			grid.removeTile(tile);
 			//console.log('d');
 		}
